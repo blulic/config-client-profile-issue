c414d9c7d95139933da546d4858b18f89a159285
Boris Lulic <boris.lulic@synchronoss.com>
2022-11-06 18:41:42 +0000

    first commit

---
+++ src/test/java/eu/borislulic/github/config/profileissue/ProfileIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.borislulic.github.config.profileissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
